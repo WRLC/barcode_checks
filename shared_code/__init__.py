392e9f789e65fd16f090e77225d1aa0969c06b15
Tom Boone <tbc@tomboone.com>
2025-05-01 10:09:39 -0400

    make imports in init absolute

--- shared_code/__init__.py
+++ shared_code/__init__.py
@@ -1,13 +1,13 @@
 """Shared code module for Alma API client and database models."""
 
-from alma_client.client import AlmaClient
-from alma_client.exceptions import AlmaClientError, AlmaApiError, AlmaClientConfigurationError
-from database import session_scope
-from database.models import (Base, APIKey, Analysis, ApiKeyPermission, AlmaApiPermission, InstitutionZone,
-                             IZAnalysisConnector, TriggerConfigIZAnalysisLink, TimerTriggerConfig,
-                             TriggerConfigUserLink)
-from utils import config_helpers, data_utils, storage_helpers
-
+from shared_code.alma_client.client import AlmaClient
+from shared_code.alma_client.exceptions import AlmaClientError, AlmaApiError, AlmaClientConfigurationError
+from shared_code.database import session_scope
+from shared_code.database.models import (
+    Base, APIKey, Analysis, ApiKeyPermission, AlmaApiPermission, InstitutionZone, IZAnalysisConnector,
+    TriggerConfigIZAnalysisLink, TimerTriggerConfig, TriggerConfigUserLink
+)
+from shared_code.utils import config_helpers, data_utils, storage_helpers
 
 __all__ = [
     'Base',
